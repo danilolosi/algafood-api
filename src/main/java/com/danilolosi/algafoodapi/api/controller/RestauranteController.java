2e114c55153548bb68bd400a29c31e8015dbe4bd
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-09-29 23:34:29 -0300

    Modelo de Usuario, Grupo, Produto e seus mapeamentos

--- src/main/java/com/danilolosi/algafoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/danilolosi/algafoodapi/api/controller/RestauranteController.java
@@ -93,7 +93,7 @@ public class RestauranteController {
 			
 			if(restauranteAtual.isPresent()) {
 				
-				BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco", "dataCadastro");
+				BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 				Restaurante restauranteSalvo = restauranteService.salvar(restauranteAtual.get());
 				return ResponseEntity.ok(restauranteSalvo);
 			}
