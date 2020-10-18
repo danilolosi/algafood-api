8891e24eb09104e66df5edd440fc9d815a9c6005
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-10-18 15:11:24 -0300

    Criando constraints de validacao customizadas usando composicao

--- src/main/java/com/danilolosi/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/danilolosi/algafoodapi/domain/model/Restaurante.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -27,6 +26,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.danilolosi.algafoodapi.core.validation.Groups;
+import com.danilolosi.algafoodapi.core.validation.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -45,9 +45,9 @@ public class Restaurante {
     @NotBlank
     @Column(nullable = false)
     private String nome;
-
-    @PositiveOrZero
+    
     @NotNull
+    @TaxaFrete
     @Column(nullable = false)
     private BigDecimal taxaFrete;
     
